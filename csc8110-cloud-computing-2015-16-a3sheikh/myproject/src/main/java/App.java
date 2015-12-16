9d338dfdb52c35bfdc76513cf9ffabe1e2582a0e
Abdullah Sheikh <a.sheikh1@newcastle.ac.uk>
2015-12-16 22:33:01 +0000

    wed 16/12/2015
    send message to service bus and created topic

--- csc8110-cloud-computing-2015-16-a3sheikh/myproject/src/main/java/App.java
+++ csc8110-cloud-computing-2015-16-a3sheikh/myproject/src/main/java/App.java
@@ -17,45 +17,33 @@ public class App
 	 
 	
 	
-	public static void main( String[] args ){
+	public static void main( String[] args ) throws ServiceException{
 
-		
-				
-		
-			/*Configuration config = ServiceBusConfiguration.configureWithSASAuthentication(
-				      "myserverone",
-				      "RootManageSharedAccessKey",
-				      "u+VrqpAFoXrISGxmiQClcc+NKLu7eolULJ8nPV3zWnw=",
-				      ".servicebus.windows.net"
-				      );
 
-				ServiceBusContract service = ServiceBusService.create(config);
-				TopicInfo topicInfo = new TopicInfo("TestTopic");
-				try  
-				{
-				    CreateTopicResult result = service.createTopic(topicInfo);
-				}
-				catch (ServiceException e) {
-				    System.out.print("ServiceException encountered: ");
-				    System.out.println(e.getMessage());
-				    System.exit(-1);
-				}
-			
-				
-				
-				SubscriptionInfo subInfo = new SubscriptionInfo("AllMessages");
-				try {
-					CreateSubscriptionResult result =
-					    service.createSubscription("TestTopic", subInfo);
-				} catch (ServiceException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}*/
-			
 				
 				int trafficRatePerMin=Integer.parseInt(args[0]);
 				
+				
 				smartSpeedCamera Cam = new smartSpeedCamera();
+				
+				System.out.println("Camera Id:\t"+Cam.getCameraId()+"\t Camera Location : \t"+ Cam.getCityLocation()+"\t Camera Street : \t"+ 
+						Cam.getStreetName()+"\t Camera Motoring Speed Limit : \t"+ Cam.getSpeedLimit()+"\t Starting Date : \t"+ Cam.getDate());
+						System.out.println("Vehicle Monitoring the following Vehicle :");
+						
+				for(int i=0; i<trafficRatePerMin ; i++){
+					
+					
+					
+					Vehicle veh = new Vehicle();
+					
+					
+					System.out.println("Vehicle :\t"+ veh.getType()+"\t\t Plate No:\t"+ veh.getPlateNo()+"\t Current Speed: \t"+veh.getCurrentSpeed());
+										
+				}
+				
+				
+				
+				Cam = new smartSpeedCamera();
 				
 				
 				System.out.println("Camera Id:\t"+Cam.getCameraId()+"\t Camera Location : \t"+ Cam.getCityLocation()+"\t Camera Street : \t"+ 
@@ -73,10 +61,53 @@ public class App
 										
 				}
 				
+				
+				
+				
+				
+				Configuration config = ServiceBusConfiguration.configureWithSASAuthentication(
+			      "myserverone",
+			      "RootManageSharedAccessKey",
+			      "u+VrqpAFoXrISGxmiQClcc+NKLu7eolULJ8nPV3zWnw=",
+			      ".servicebus.windows.net"
+			      );
+
+			ServiceBusContract service = ServiceBusService.create(config);
+		   TopicInfo topicInfo = new TopicInfo("CameraRecord");
+			try  
+			{
+			    CreateTopicResult result = service.createTopic(topicInfo);
+			}
+			catch (ServiceException e) {
+			    System.out.print("ServiceException encountered: ");
+			    System.out.println(e.getMessage());
+			    System.exit(-1);
+			}
 		
+			
+			// Create message, passing a string message for the body
+		    BrokeredMessage message = new BrokeredMessage("camera message ");
+		    // Set some additional custom app-specific property
+		    message.setProperty("Camera Id", Cam.getCameraId());
+		    message.setProperty("Camera Location", Cam.getCityLocation());
+		    message.setProperty("Camera Street", Cam.getStreetName());
+		    message.setProperty("Camera Speed Limit", Cam.getSpeedLimit());
+		    // Send message to the topic
+		   
+			service.sendTopicMessage("CameraRecord", message);
+			
 
+			
+			
+			
+			/*SubscriptionInfo subInfo = new SubscriptionInfo("AllMessages");
+			try {
+				CreateSubscriptionResult result =  service.createSubscription("TestTopic", subInfo);
+			} catch (ServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/				
+				
+				
 					
-
-
-
 }}
