111d8bbe24090ff47435c370764197054a26c6f1
Abdullah Sheikh <a.sheikh1@newcastle.ac.uk>
2015-12-17 18:59:10 +0000

    17/12/2015
    Receiving messages from topic

--- csc8110-cloud-computing-2015-16-a3sheikh/myproject/src/main/java/App.java
+++ csc8110-cloud-computing-2015-16-a3sheikh/myproject/src/main/java/App.java
@@ -19,18 +19,73 @@ public class App
 	
 	public static void main( String[] args ) throws ServiceException{
 
-
+		// create config to connect with service Bus
+		Configuration config = ServiceBusConfiguration.configureWithSASAuthentication(
+	      "myserverone",
+	      "RootManageSharedAccessKey",
+	      "u+VrqpAFoXrISGxmiQClcc+NKLu7eolULJ8nPV3zWnw=",
+	      ".servicebus.windows.net"
+	      );
+		
+		ServiceBusContract service = ServiceBusService.create(config);
+		// this code to delete any topcs and subscription exist for testing 
+		service.deleteSubscription("CameraRecord", "Monitoring");
+		service.deleteTopic("CameraRecord");
+		
+		//to create topic with size 5GB
+		long maxSizeInMegabytes = 5120;
+		TopicInfo topicInfo = new TopicInfo("CameraRecord");
+		topicInfo.setMaxSizeInMegabytes(maxSizeInMegabytes);
+		try  
+		{
+		    CreateTopicResult result = service.createTopic(topicInfo);
+		}
+		catch (ServiceException e) {
+		   System.out.print("ServiceException encountered: ");
+		    System.out.println(e.getMessage());
+		    System.exit(-1);
+		}
+	
+		//crate subscription 
+		SubscriptionInfo subInfo = new SubscriptionInfo("Monitoring");
+		CreateSubscriptionResult result1 =  service.createSubscription("CameraRecord", subInfo);
+		
+		//define traffic Rate Per Min reads from arsg[0] 		
+		int trafficRatePerMin=Integer.parseInt(args[0]);
 				
-				int trafficRatePerMin=Integer.parseInt(args[0]);
 				
-				
+		//crate Speed Camera with random information 
 				smartSpeedCamera Cam = new smartSpeedCamera();
-				
+			// print camera information 
 				System.out.println("Camera Id:\t"+Cam.getCameraId()+"\t Camera Location : \t"+ Cam.getCityLocation()+"\t Camera Street : \t"+ 
 						Cam.getStreetName()+"\t Camera Motoring Speed Limit : \t"+ Cam.getSpeedLimit()+"\t Starting Date : \t"+ Cam.getDate());
 						System.out.println("Vehicle Monitoring the following Vehicle :");
 						
-				for(int i=0; i<trafficRatePerMin ; i++){
+						
+						// submitting camera information to the topic 
+						// Create message, passing a string message for the body
+					    BrokeredMessage message = new BrokeredMessage("cameramessage");
+
+					    // Set some additional custom app-specific property
+					   message.setProperty("CameraId", Cam.getCameraId());
+						
+						
+					    message.setProperty("CameraLocation", Cam.getCityLocation());
+						
+						
+					    message.setProperty("CameraStreet", Cam.getStreetName());
+						
+						
+					   message.setProperty("CameraSpeedLimit", Cam.getSpeedLimit());
+					  
+					   service.sendTopicMessage("CameraRecord", message);		 // Send message to the topic	
+
+					   
+					   
+					   
+					   
+				// create Vehicles Monitored by speed camera depends on the traffic rate reads from args.
+					   for(int i=0; i<trafficRatePerMin ; i++){
 					
 					
 					
@@ -38,143 +93,83 @@ public class App
 					
 					
 					System.out.println("Vehicle :\t"+ veh.getType()+"\t\t Plate No:\t"+ veh.getPlateNo()+"\t Current Speed: \t"+veh.getCurrentSpeed());
-										
+					//adding vehicle information to current speed camera of the same topic
+					 message.setProperty("VehicleType", veh.getType());
+					 message.setProperty("VehiclePlateNo", veh.getPlateNo());
+					 message.setProperty("VehicleCurrentSpeed", veh.getCurrentSpeed());
+					 service.sendTopicMessage("CameraRecord", message);
+					
 				}
 				
 				
 				
-				/*Cam = new smartSpeedCamera();
 				
-				
-				System.out.println("Camera Id:\t"+Cam.getCameraId()+"\t Camera Location : \t"+ Cam.getCityLocation()+"\t Camera Street : \t"+ 
-						Cam.getStreetName()+"\t Camera Motoring Speed Limit : \t"+ Cam.getSpeedLimit()+"\t Starting Date : \t"+ Cam.getDate());
-						System.out.println("Vehicle Monitoring the following Vehicle :");
-						
-				for(int i=0; i<trafficRatePerMin ; i++){
-					
-					
-					
-					Vehicle veh = new Vehicle();
-					
-					
-					System.out.println("Vehicle :\t"+ veh.getType()+"\t\t Plate No:\t"+ veh.getPlateNo()+"\t Current Speed: \t"+veh.getCurrentSpeed());
-										
-				}*/
-				
-				
-				
-				
-				
-				Configuration config = ServiceBusConfiguration.configureWithSASAuthentication(
-			      "myserverone",
-			      "RootManageSharedAccessKey",
-			      "u+VrqpAFoXrISGxmiQClcc+NKLu7eolULJ8nPV3zWnw=",
-			      ".servicebus.windows.net"
-			      );
-				
+		   // This code will print all Vehicle monitored by a speed camera and the camera information from the messages 
+		   //that sent to the topic and from the subscription adopted from practical materials and modified 
+					   try
+					   {
+					       ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
+					       opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
 
-			ServiceBusContract service = ServiceBusService.create(config);
-		  
-			service.deleteSubscription("CameraRecord", "Monitoring");
-			service.deleteTopic("CameraRecord");
-			
-			
-			long maxSizeInMegabytes = 5120;
-			TopicInfo topicInfo = new TopicInfo("CameraRecord");
-			topicInfo.setMaxSizeInMegabytes(maxSizeInMegabytes);
-			try  
-			{
-			    CreateTopicResult result = service.createTopic(topicInfo);
-			}
-			catch (ServiceException e) {
-			   System.out.print("ServiceException encountered: ");
-			    System.out.println(e.getMessage());
-			    System.exit(-1);
-			}
-		
-			
-			SubscriptionInfo subInfo = new SubscriptionInfo("Monitoring");
-			CreateSubscriptionResult result1 =  service.createSubscription("CameraRecord", subInfo);
-			
-			
-			// Create message, passing a string message for the body
-		    BrokeredMessage message = new BrokeredMessage("cameramessage");
-
-		    // Set some additional custom app-specific property
-		   message.setProperty("CameraId", Cam.getCameraId());
-			
-			
-		    message.setProperty("CameraLocation", Cam.getCityLocation());
-			
-			
-		    message.setProperty("CameraStreet", Cam.getStreetName());
-			
-			
-		   message.setProperty("CameraSpeedLimit", Cam.getSpeedLimit());
-		  
-		   service.sendTopicMessage("CameraRecord", message);		 // Send message to the topic	
-
-			
-			
-			
-			/*SubscriptionInfo subInfo = new SubscriptionInfo("AllMessages");
-			try {
-				CreateSubscriptionResult result =  service.createSubscription("TestTopic", subInfo);
-			} catch (ServiceException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/				
-				
+					       while(true)  {
+					           ReceiveSubscriptionMessageResult  resultSubMsg = service.receiveSubscriptionMessage("CameraRecord", "Monitoring", opts);
+					           message = resultSubMsg.getValue();
+					           if (message != null && message.getProperty("CameraId") != null)
+					           {
+					               
+					               // Display the topic message.
+					               System.out.print("From topic: "+"CameraRecord");
+					               byte[] b = new byte[200];
+					               String s = null;
+					               int numRead = message.getBody().read(b);
+					               while (-1 != numRead)
+					               {
+					                   s = new String(b);
+					                   s = s.trim();
+					                  
+					                   numRead = message.getBody().read(b);
+					               }
+					             
+					              
+					               System.out.println();
+					               System.out.println("CameraId: " + message.getProperty("CameraId"));
+					               System.out.println("CameraLocation: " + message.getProperty("CameraLocation"));
+					               System.out.println("CameraStreet: " + message.getProperty("CameraStreet"));
+					               System.out.println("CameraSpeedLimit: " + message.getProperty("CameraSpeedLimit"));
+					           					            	  					              
+					               System.out.println();
+					               System.out.println("VehicleType: " + message.getProperty("VehicleType"));
+					               System.out.println("VehiclePlateNo: " + message.getProperty("VehiclePlateNo"));
+					               System.out.println("VehicleCurrentSpeed: " + message.getProperty("VehicleCurrentSpeed"));
+					               // Delete message.
+					               System.out.println("Deleting this message.");
+					               service.deleteMessage(message);
+					           }  
+					           
+					           
+					          
+					           else  
+					           {
+					               System.out.println("Finishing up - no more messages.");
+					               break;
+					              
+					           }
+					       }
+					   }
+					   catch (ServiceException e) {
+					       System.out.print("ServiceException encountered: ");
+					       System.out.println(e.getMessage());
+					       System.exit(-1);
+					   }
+					   catch (Exception e) {
+					       System.out.print("Generic exception encountered: ");
+					       System.out.println(e.getMessage());
+					       System.exit(-1);
+					   }
 		   
-		   try
-		   {
-		       ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
-		       opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
-
-		       while(true)  {
-		           ReceiveSubscriptionMessageResult  resultSubMsg = service.receiveSubscriptionMessage("CameraRecord", "HighMessages", opts);
-		           message = resultSubMsg.getValue();
-		           if (message != null && message.getMessageId() != null)
-		           {
-		               System.out.println("MessageID: " + message.getMessageId());
-		               // Display the topic message.
-		               System.out.print("From topic: ");
-		               byte[] b = new byte[200];
-		               String s = null;
-		               int numRead = message.getBody().read(b);
-		               while (-1 != numRead)
-		               {
-		                   s = new String(b);
-		                   s = s.trim();
-		                   System.out.print(s);
-		                   numRead = message.getBody().read(b);
-		               }
-		               System.out.println();
-		               System.out.println("Custom Property: " +
-		                   message.getProperty("MessageNumber"));
-		               // Delete message.
-		               System.out.println("Deleting this message.");
-		               service.deleteMessage(message);
-		           }  
-		           else  
-		           {
-		               System.out.println("Finishing up - no more messages.");
-		               break;
-		               // Added to handle no more messages.
-		               // Could instead wait for more messages to be added.
-		           }
-		       }
-		   }
-		   catch (ServiceException e) {
-		       System.out.print("ServiceException encountered: ");
-		       System.out.println(e.getMessage());
-		       System.exit(-1);
-		   }
-		   catch (Exception e) {
-		       System.out.print("Generic exception encountered: ");
-		       System.out.println(e.getMessage());
-		       System.exit(-1);
-		   }
+		   
+		   
+		   
 
 		   
 		   
